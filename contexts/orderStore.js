10dc1d6f7bb3168fd827aa2376b8a356cbc1422f
Jashank <jashankjain224@gmail.com>
2024-05-14 13:14:00 +0530

    change

--- contexts/orderStore.js
+++ contexts/orderStore.js
@@ -1,10 +1,10 @@
 import {create} from 'zustand';
 
 const useOrderStore = create((set) => ({
-  dailyOrderCount: 1,
+  dailyOrderCount: 0,
   incrementDailyOrderCount: () =>
     set((state) => ({ dailyOrderCount: state.dailyOrderCount + 1 })),
-  resetDailyOrderCount: () => set({ dailyOrderCount: 1 }),
+  resetDailyOrderCount: () => set({ dailyOrderCount: 0 }),
 }));
 
 export default useOrderStore;
