92f0e0c7b498b622bacad2e4cc3c94092ddc0a6a
Jashank <jashankjain224@gmail.com>
2024-05-15 01:44:44 +0530

    first_prototype

--- src/app/liveorders/page.js
+++ src/app/liveorders/page.js
@@ -4,16 +4,31 @@ import Navbar from '../components/Navbar';
 import useOrderStore from '../../../contexts/orderStore';
 import Livecard from '../components/Livecard';
 import Ordereadycard from '../components/Ordereadycard';
+import {io} from 'socket.io-client';
+
 const page = () => {
-
-
-  // const { dailyOrderCount , resetDailyOrderCount , incrementDailyOrderCount } = useOrderStore();
-
-  const [orderData, setOrderData] = useState(null);
+  
+  
+  const { dailyOrderCount , incrementDailyOrderCount } = useOrderStore();
+  const [socket,setsocket] = useState(undefined)
+  const [orders, setOrders] = useState([]);
  
     // incrementDailyOrderCount();
-   
-
+    useEffect(() => { 
+     const  socket = io('https://afosfr-server.onrender.com/'); //https://afosfr-server.onrender.com/
+      setsocket(socket);
+  
+      socket.on("fetchOrder",(data)=>{
+        incrementDailyOrderCount();
+        setOrders(prevOrders => [...prevOrders, data]);
+        // setOrders(prevOrders => [...prevOrders, { ...data, dailyOrderCount: dailyOrderCount+1 }]);
+        // console.log(orders);
+        
+       })
+       return () => {
+        socket.off("fetchOrder"); // Cleanup socket event listener on component unmount
+      };
+    },[]);
     
   return (
     <>
@@ -28,23 +43,27 @@ const page = () => {
 
     {/* live orderss here */}
     <h1 className='mb-3 text-center text-lg  font-semibold tracking-wide text-white'>Live orders</h1>
-    <div className='flex  mx-4 my-4 h-[40vh] overflow-x-auto overflow-y-hidden scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-zinc-400 scrollbar-track-zinc-900 scrollbar-thin scrollbar-corner-zinc-300  '>
+    <div className='flex flex-row mx-4 my-4 h-[44vh] overflow-x-auto overflow-y-hidden scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-zinc-400 scrollbar-track-zinc-900 scrollbar-thin scrollbar-corner-zinc-300  '>
 
-    <div>
-      {/* <Livecard
-        orderId={orderData.orderId}
-        name={orderData.name}
-        order={orderData.order}
-        amount={orderData.amount}
-        dailycount={orderData.dailycount}
-      /> */}
-  </div>
+   
+          {orders.map((order, index) => (
+              <Livecard
+                key={index} // Assign a unique key for each Livecard
+                orderId={order.orderId}
+                name={order.name}
+                order={order.orders.map(item => `${item.quantity} * ${item.itemName}`).join(", ")}
+                amount={order.amount}
+                dailycount={dailyOrderCount}
+                // dailycount={order.dailyOrderCount}
+              />
+            ))}
+ 
     
     </div>
     {/* orders completed here */}
     <div className='my-2 ml-4'> <hr /></div>
     <h1 className='mb-3 text-center text-lg  font-semibold tracking-wide text-white'>Orders Ready</h1>
-    <div className='flex  mx-4 my-1 h-[34vh] bg-yellow-50 '>
+    <div className='flex  mx-4 my-1 h-[34vh]  '>
     <Ordereadycard/>
     </div>
 
--- src/app/sockets/page.js
+++ src/app/sockets/page.js
@@ -1,37 +1,37 @@
 // pages/index.js
-"use client"
-import { useEffect,useState} from 'react';
-import {io} from 'socket.io-client';
+// "use client"
+// import { useEffect,useState} from 'react';
+// import {io} from 'socket.io-client';
 
-export default function Home() {
+// export default function Home() {
 
-  const [socket,setsocket] = useState(undefined)
-  const [orderdata,setorderdata] = useState("ji");
-  const data = "1* pizza";
+//   const [socket,setsocket] = useState(undefined)
+//   const [orderdata,setorderdata] = useState("ji");
+//   const data = "1* pizza";
 
-  useEffect(() => {
-    const socket = io('https://afosfr-server.onrender.com/');
-    setsocket(socket);
+  // useEffect(() => {
+  //   const socket = io('http://localhost:5000/');
+  //   setsocket(socket);
 
-    socket.on("newOrder",(data)=>{
-      setorderdata(data);
-     })
+  //   socket.on("newOrder",(data)=>{
+  //     setorderdata(data);
+  //    })
 
-  }, []);
+  // }, []);
 
     
-   const handleclick = ()=>{
-    socket.emit("newOrder" , data);
-   }
+  //  const handleclick = ()=>{
+  //   socket.emit("newOrder" , data);
+  //  }
 
    
 
-  return (
-    <div>
-      <h1>Socket.IO Client</h1>
-      <p>{orderdata}</p>
-      <button onClick={handleclick} className='bg-red'> click me </button>
-      {/* <button onClick={handlegetme} className='bg-red'> get me </button> */}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>Socket.IO Client</h1>
+//       <p>{orderdata}</p>
+//       <button onClick={handleclick} className='bg-red'> click me </button>
+//       {/* <button onClick={handlegetme} className='bg-red'> get me </button> */}
+//     </div>
+//   );
+// }
